9432db4aef8e6b3b52dcb2998be8e1540ec97cae
ksaanen <ksaanen@independer.nl>
2019-10-15 15:50:21 +0200

    refactor a bit

--- src/tooltips.ts
+++ src/tooltips.ts
@@ -1,53 +1,99 @@
 namespace SomeNamespace.Vanilla.Core {
 
-  export interface Tooltip {
+  export class Tooltip {
     header: string;
     content: string;
-    open?: boolean;
     refElement: string;
+    
+    private _isOpen: boolean;
+    private _ref: HTMLElement;
+    
+    constructor(tooltip: Tooltip) {
+      this.ref = document.querySelector(tooltip.refElement);
+
+      this.ref.onclick = () => {
+        this.onClick();
+      };
+    }
+
+    positionTooltip() {
+      console.log('bla', this.ref);
+    }
+
+    private onClick() {
+      // Set isOpen
+      if (!this.isOpen) {
+        this.isOpen = true;
+      }
+
+      this.show();
+    }
+
+    private show() {
+      let el = document.createElement('div');
+      el.classList.add('tooltip');
+      el.innerHTML = `
+        <div class="header">${this.header}</div>
+        <div class="content">${this.content}</div>
+      `;
+      return el;
+    }
+
+    
+    get isOpen(): boolean {
+      return this._isOpen;
+    }
+
+    set isOpen(bool: boolean) {
+      this._isOpen = bool;
+    }
+
+    get ref(): HTMLElement {
+      return this._ref;
+    }
+
+    set ref(el: HTMLElement) {
+      this._ref = el;
+    }
+
   }
 
   export class Tooltips {
 
-    private tooltipsArray: Tooltip[];
-    private isOpen: boolean;
+    private tooltips: Tooltip[] = [];
 
     constructor() {
-      this.tooltipsArray = [];
-
       this.init();
     }
 
     // Accept a Tooltip object or an array of Tooltip objects
-    add(tooltips: Tooltip | Tooltip[]) {
-      if (Array.isArray(tooltips)) {
-        this.tooltipsArray.push(...tooltips);
-      }
-      else if (typeof tooltips === 'object') {
-        this.tooltipsArray.push(tooltips);
-      }
+    addTooltip(tooltip: Tooltip) {
+      let t = new Tooltip(tooltip);
+      this.tooltips.push(t);
     }
 
-    init() {
-      
+    private init() {
+
       // Add resize eventlistener to window
       window.addEventListener('resize', (e) => {
         this.onResize();
       });
     }
 
+    private getOpenTooltip(): Tooltip {
+      // Returns first found open tooltip (tooltips are expected to have 1 open at a time).
+      return this.tooltips.filter(function (tooltip) {
+        return tooltip.isOpen === true;
+      })[0];
+    }
+
     private onResize() {
-
-    }
-
-    private show() {
-
-    }
-
-    private render() {
-
+      // First check if a tooltip has opened.
+      if (this.getOpenTooltip()) {
+        this.getOpenTooltip().positionTooltip();
+      }
     }
 
   }
-  
+
 }
